9ca6f74d342711585cde125d9b07cf9d1dce5f33
Paul Prins <paulprins@gmail.com>
2021-10-12 13:51:52 +0200

    Update shell scripts to ensure they work when called from any location

--- docker/start.sh
+++ docker/start.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_DIR="$(dirname -- "$(realpath -- "$SCRIPT_DIR")")"
+
+echo
+echo
+echo "STARTING CONTAINERS FOR PROJECT: UrbanMonastics/Source-Text-Library"
+echo
+
 
 # validate our current working directory has no spaces
-D=`pwd`;
-if [[ "$D" =~ " " ]]
+if [[ "$PROJECT_DIR" =~ " " ]]
 then
-	echo "You have a space in your pwd. Please cd into a path without any spaces."
+	echo "You have a space in your directory path. Please use a path without any spaces."
 	exit 1
 fi
 
+
 # start in the background
-docker-compose up -d
+docker-compose -f "$PROJECT_DIR"/docker-compose.yml up  -d
 
 echo
 echo "You can connect from your development (host) machine to these services:"
--- docker/stop.sh
+++ docker/stop.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker-compose stop
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_DIR="$(dirname -- "$(realpath -- "$SCRIPT_DIR")")"
+
+echo
+echo
+echo "STOPPING CONTAINERS FOR PROJECT: UrbanMonastics/Source-Text-Library"
+echo
+
+# stop project containers
+docker-compose -f "$PROJECT_DIR"/docker-compose.yml stop
